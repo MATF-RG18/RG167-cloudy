fc94108c6a65c0298fc11cf35429099eec03b26a
Vera Milosavljevic <vera.matf@gmail.com>
2019-08-23 22:31:46 +0200

    Dodato kretanje oblaka, oblaci su crne i bele boje, ograniceno kretanje topa

--- game.cpp
+++ game.cpp
@@ -10,14 +10,15 @@
 
 #define PI 3.141592653589
 #define DEG2RAD(deg) (deg * PI / 180)
-
+#define TIMER_ID 0
+#define TIMER_INTERVAL 20
 
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
 static void on_display(void);
 static void on_mouse(int button, int state, int x, int y);
 static void on_motion(int x, int y);
-
+static void pomeranjeOblaka(int value);
 void pocetneVrednosti();
 void nacrtajPostolje(void);
 void nacrtajElipsu(double centerX, double centerY, double radiusX, double radiusY);
@@ -25,12 +26,14 @@ void nacrtajOblak();
 void nacrtajTop();
 
 
-
 static int window_width, window_height;
 static std::vector<double> xKoordinateOblaka(1000), yKoordinateOblaka(1000);
+static std::vector<double> brzinaOblakaPoX(1000), brzinaOblakaPoY(1000);
+static std::vector<int> bojaOblaka(1000);
+static int kretanjeOblaka;
 static int mouse_x, mouse_y; /* Koordinate misa. */
 static float matrix[16];/*kumulativna matrica rotacije*/
-static double pomerajPoX;
+static double pomerajPoX, rotacijaUgla;
 
 int main(int argc, char** argv){
     
@@ -39,8 +42,8 @@ int main(int argc, char** argv){
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
 
     /* Kreira se prozor. */
-    glutInitWindowSize(700, 700);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(700, 600);
+    glutInitWindowPosition(0, 0);
     glutCreateWindow(argv[0]);
     
     pocetneVrednosti();
@@ -51,6 +54,7 @@ int main(int argc, char** argv){
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
 
+    pocetneVrednosti();
     
     /* Obavlja se OpenGL inicijalizacija. */
     glClearColor(0.75, 0.85, 0.85, 0);
@@ -64,6 +68,10 @@ int main(int argc, char** argv){
 
 void pocetneVrednosti(){
     pomerajPoX = 0.0;
+    rotacijaUgla = 45;
+    kretanjeOblaka = 0;
+    double pocetnaBrzinaPoX = 0.03;
+    double pocetnaBrzinaPoY = 0.003;
     srand(time(NULL));
     for(int i = 0; i<100; i++){
         
@@ -76,6 +84,22 @@ void pocetneVrednosti(){
         } else {
             xKoordinateOblaka.at(i) = xKoordinata;
         }
+        double smerKretanja = rand()/(float)RAND_MAX;
+        if(smerKretanja < 0.5){
+            brzinaOblakaPoX.at(i) = pocetnaBrzinaPoX;
+        } else {
+            brzinaOblakaPoX.at(i) = -pocetnaBrzinaPoX;
+        }
+        brzinaOblakaPoY.at(i) = pocetnaBrzinaPoY;
+        if(i!=0 && i%3 == 0){
+            pocetnaBrzinaPoX += 0.01;
+            pocetnaBrzinaPoY += 0.001;
+        }
+        double indikatorZaBoju = rand()/(float)RAND_MAX;
+        if(indikatorZaBoju < 0.5)
+            bojaOblaka[i] = 1;
+        else
+            bojaOblaka[i] = 0;
     }
     
     /*inicijalizacija matrice rotacije*/
@@ -96,7 +120,7 @@ static void on_motion(int x, int y){
     int deltaX;
     
     deltaX = x - mouse_x;
-
+    
     mouse_x = x;
     mouse_y = y;
     
@@ -104,9 +128,13 @@ static void on_motion(int x, int y){
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
         glLoadIdentity();
-        pomerajPoX += deltaX * 0.003;
-
-            
+        
+        if(pomerajPoX <= 1.8 && pomerajPoX >=-1.8)
+            pomerajPoX += deltaX * 0.005;
+        else if(pomerajPoX > 1.8)
+            pomerajPoX = 1.8;
+        else
+            pomerajPoX = -1.8;
         glRotatef(pomerajPoX, 1, 0, 0);
         glMultMatrixf(matrix);        
     glPopMatrix();
@@ -123,8 +151,16 @@ static void on_keyboard(unsigned char key, int x, int y){
     switch (key) {
     case 27:
         exit(0);
+        break;        
+    case 's':
+    case 'S':
+        if (!kretanjeOblaka) {
+            glutTimerFunc(TIMER_INTERVAL, pomeranjeOblaka, TIMER_ID);
+            kretanjeOblaka = 1;
+        }
         break;
     }
+
 }
 
 
@@ -148,16 +184,34 @@ static void on_display(void){
     glEnable(GL_DEPTH_TEST);
     nacrtajOblak();
     nacrtajPostolje();
+    
     glMultMatrixf(matrix);
     nacrtajTop();
     glutSwapBuffers();
 }
 
+static void pomeranjeOblaka(int value){
+
+    if (value != TIMER_ID)
+        return;
+    unsigned int i = 0;
+    for(i = 0; i<100; i++){
+        xKoordinateOblaka.at(i) += brzinaOblakaPoX.at(i);
+        yKoordinateOblaka.at(i) -= brzinaOblakaPoY.at(i);
+        if(xKoordinateOblaka.at(i) + 0.2 >= 2 || xKoordinateOblaka.at(i) - 0.2 <= -2)
+            brzinaOblakaPoX.at(i) *= -1;
+    }
+    glutPostRedisplay();
+    
+    if (kretanjeOblaka) {
+        glutTimerFunc(TIMER_INTERVAL, pomeranjeOblaka, TIMER_ID);
+    }
+}
 
 
 void nacrtajPostolje(){
     glDisable(GL_LIGHTING);
-    GLfloat pozicija_osvetljenja[] = { 1, 10, 7, 1 };
+    GLfloat pozicija_osvetljenja[] = { 1, 10, 8, 1 };
 
     GLfloat ambijentalno_osvetljenje[] = { 0.35, 0.35, 0.35, 1 };
     GLfloat difuzno_osvetljenje[] = { 0.75, 0.75, 0.75, 1 };
@@ -183,7 +237,7 @@ void nacrtajPostolje(){
     glMaterialf(GL_FRONT, GL_SHININESS, shininess);
 
     glPushMatrix();
-            glTranslatef(0, -1.4, 0);
+            glTranslatef(0, -1.4, 1);
             glScalef(3.5, 0.5, 1);
             glutSolidCube(1);
     glPopMatrix();
@@ -198,7 +252,6 @@ void nacrtajElipsu(double centerX, double centerY, double radiusX, double radius
             float degInRad = i * DEG2RAD;
             glVertex2d (centerX + cos (degInRad) * radiusX*0.3, 
                             centerY + sin (degInRad) * radiusY*0.3);
-            
         }
         
     glEnd();
@@ -208,26 +261,26 @@ void nacrtajElipsu(double centerX, double centerY, double radiusX, double radius
 void nacrtajOblak(){
     
     glDisable(GL_LIGHTING);
-
+    
     for(int i = 0; i<100; i++){
         double x = xKoordinateOblaka.at(i);
         double y = yKoordinateOblaka.at(i);
-        glColor3f(255.0, 255.0, 255.0);
+        glColor3f(bojaOblaka[i], bojaOblaka[i], bojaOblaka[i]);
         nacrtajElipsu(x, y, 1.2, 0.7);
-        glColor3f(255.0, 255.0, 255.0);
+        glColor3f(bojaOblaka[i], bojaOblaka[i], bojaOblaka[i]);
         nacrtajElipsu(x+0.23, y+0.12, 0.35, 0.35);
-        glColor3f(255.0, 255.0, 255.0);
+        glColor3f(bojaOblaka[i], bojaOblaka[i], bojaOblaka[i]);
         nacrtajElipsu(x+0.23, y-0.12, 0.35, 0.35);
-        glColor3f(255.0, 255.0, 255.0);
+        glColor3f(bojaOblaka[i], bojaOblaka[i], bojaOblaka[i]);
         nacrtajElipsu(x-0.23, y-0.12, 0.35, 0.35);
-        glColor3f(255.0, 255.0, 255.0);
+        glColor3f(bojaOblaka[i], bojaOblaka[i], bojaOblaka[i]);
         nacrtajElipsu(x-0.23, y+0.12, 0.35, 0.35);
     }
 }
 
 void nacrtajTop(){
-    glDisable(GL_LIGHTING);
-    GLfloat pozicija_osvetljenja[] = { 1, 10, 7, 1 };
+        
+    GLfloat pozicija_osvetljenja[] = { 1, 10, 8, 1 };
 
     GLfloat ambijentalno_osvetljenje[] = { 0.35, 0.35, 0.35, 1 };
     GLfloat difuzno_osvetljenje[] = { 0.75, 0.75, 0.75, 1 };
@@ -254,21 +307,23 @@ void nacrtajTop(){
     glPushMatrix();
         GLUquadricObj *quadratic;
         quadratic = gluNewQuadric();
-        glRotatef(45, 1, 0, 0);
-        glTranslatef(pomerajPoX, -0.2, 0.3);
+        glTranslatef(pomerajPoX, -0.4, 1.15);
         
-        gluCylinder(quadratic, 0.1f, 0.1f, 0.7f, 32, 32);
+        glRotatef(45, 1, 0, 0);
+        
+        
+        gluCylinder(quadratic, 0.1f, 0.1f, 0.7f, 72, 72);
     glPopMatrix();        
     
     glPushMatrix();
         GLUquadricObj *quadratic1;
         quadratic1 = gluNewQuadric();
         glRotatef(90, 0, 1, 0);
-        glTranslatef(-0.4, -0.95, pomerajPoX+0.1);
+        glTranslatef(-1.4, -0.95, pomerajPoX+0.1);
         
-        gluCylinder(quadratic1, 0.2f, 0.2f, 0.1f, 32, 32);
+        gluCylinder(quadratic1, 0.2f, 0.2f, 0.1f, 72, 72);
 
         glTranslatef(0, 0, -0.3);
-        gluCylinder(quadratic1, 0.2f, 0.2f, 0.1f, 32, 32);
+        gluCylinder(quadratic1, 0.2f, 0.2f, 0.1f, 72, 72);
     glPopMatrix();
 }
